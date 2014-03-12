a7fb7500283682c333c539aef96a839ed0083c8d
Tonny <vlcek.a@email.cz>
2014-03-12 23:17:25 +0100

    Vytvoření struktury aplikace - o vykreslování domovské stránky se nyní stará objekt 'HomeView' umístěný ve stejnojmeném js souboru. Implementace ovlivnila zdrojové kódy 'idex.html' a 'main.js'

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 var app = {
-
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -13,17 +6,12 @@ var app = {
             alert(title ? (title + ": " + message) : message);
         }
     },
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
     initialize: function() {
         var self = this;
         this.homeTpl = Handlebars.compile($("#home-tpl").html());
         this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         this.store = new MemoryStore(function (){
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
             self.showAlert('This application is ready', 'Info');
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
